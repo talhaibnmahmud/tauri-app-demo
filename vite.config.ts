7cfd24461b744e7042b7668c34cfdb4692cc278f
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-10-02 16:08:50 +0600

    🚀 Setup Relative Paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -16,6 +16,12 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
 }));
